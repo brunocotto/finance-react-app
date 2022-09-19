ad41a1f841172582c0b63a9c30c963c8e141e453
brunocotto <bruno.ottoo@gmail.com>
2022-09-19 19:30:20 -0300

    Componente Sidebar(menu)  finalizado

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ function App() {
   return (
     <div>
       <Sidebar />
+      <Dashboard />
+      <Sidebar />    
     </div>
   );
 }
 
 export default App;
+
+const Div = styled.div `
+position: relative;
+`;
