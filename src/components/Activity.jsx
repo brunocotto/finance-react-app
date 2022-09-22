ab6b9ed3df8450db75fd2d08cbe1e136a3fe5e29
brunocotto <bruno.ottoo@gmail.com>
2022-09-22 00:05:40 -0300

    componente payment inicializado

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -8,78 +8,78 @@ import { AiOutlineWifi } from "react-icons/ai"
 function Activity() {
   return (
 <Section>
-          <div className='title'>
-              <h4>Recent Activities</h4>
-              <h6>10 Jan 2022</h6>
-          </div>
-      <div className="analytic ">
-          <div className="design">
-              <div className="logo">
-                  <MdOutlineWaterDrop />
-              </div>
-              <div className="content">
-                  <h5>Water Bill</h5>
-                  <h5 className='color'>Successfully</h5>
-              </div>
-              
-          </div>
-          <div className="money">
-                  <h5>$120</h5>
-                  
-              </div>
-        
-      </div>
-      <div className="analytic ">
-          <div className="design">
-              <div className="logo">
-                  <GiPayMoney />
-              </div>
-              <div className="content">
-                  <h5>Income Salary</h5>
-                  <h5 className='color'>Received</h5>
-              </div>
-              
-          </div>
-          <div className="money">
-                  <h5>$4500</h5>
-                  
-              </div>
-        
-      </div>
-      <div className="analytic ">
-          <div className="design">
-              <div className="logo">
-                  <AiOutlineThunderbolt />
-              </div>
-              <div className="content">
-                  <h5>Electric Bill</h5>
-                  <h5 className='color'>Successfully</h5>
-              </div>
-              
-          </div>
-          <div className="money">
-                  <h5>$150</h5>
-                  
-              </div>
-        
-      </div>
-      <div className="analytic ">
-          <div className="design">
-              <div className="logo">
-                  <AiOutlineWifi />
-              </div>
-              <div className="content">
-                  <h5>Internet Bill</h5>
-                  <h5 className='color'>Successfully</h5>
-              </div>
-              
-          </div>
-          <div className="money">
-                  <h5>$60</h5>
-                  
-              </div>
-        
-      </div>  
+        <div className='title'>
+            <h4>Recent Activities</h4>
+            <h6>10 Jan 2022</h6>
+        </div>
+    <div className="analytic ">
+        <div className="design">
+            <div className="logo">
+                <MdOutlineWaterDrop />
+            </div>
+            <div className="content">
+                <h5>Water Bill</h5>
+                <h5 className='color'>Successfully</h5>
+            </div>
+            
+        </div>
+        <div className="money">
+                <h5>$120</h5>
+                
+            </div>
+      
+    </div>
+    <div className="analytic ">
+        <div className="design">
+            <div className="logo">
+                <GiPayMoney />
+            </div>
+            <div className="content">
+                <h5>Income Salary</h5>
+                <h5 className='color'>Received</h5>
+            </div>
+            
+        </div>
+        <div className="money">
+                <h5>$4500</h5>
+                
+            </div>
+      
+    </div>
+    <div className="analytic ">
+        <div className="design">
+            <div className="logo">
+                <AiOutlineThunderbolt />
+            </div>
+            <div className="content">
+                <h5>Electric Bill</h5>
+                <h5 className='color'>Successfully</h5>
+            </div>
+            
+        </div>
+        <div className="money">
+                <h5>$150</h5>
+                
+            </div>
+      
+    </div>
+    <div className="analytic ">
+        <div className="design">
+            <div className="logo">
+                <AiOutlineWifi />
+            </div>
+            <div className="content">
+                <h5>Internet Bill</h5>
+                <h5 className='color'>Successfully</h5>
+            </div>
+            
+        </div>
+        <div className="money">
+                <h5>$60</h5>
+                
+            </div>
+      
+    </div>  
     </Section>
   );
 }
